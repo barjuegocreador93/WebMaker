16bf1fdddaf568bbaa95df5bab037c04f7cb9dc4
Camilo Barbosa <barbosa93cg@gmail.com>
2015-09-23 16:55:21 -0500

    update 1.1
    Remove elements

--- js/editorCreator.js
+++ js/editorCreator.js
@@ -4,8 +4,9 @@
  * and open the template in the editor.
  */
 
-objets=0;
-select="";
+var objets=0;
+var select="";
+
 
 
 
@@ -14,31 +15,81 @@ function selectObj(elem,name)
     $(elem).click(function()
         {
             no=0;
+            $('#tablero').css("border-color","black");
             for(var i=0;looking(name+i+"");i++)
             {
+                
                 if(isJQ(name+i+"",":hover")&&!isJQ(elem,name+i+"")&&!isJQ(name+i+" "+elem,":hover"))
                 {
                     no++;
+                    
+                }
+                else
+                {
+                    $(name+i+"").css("border-color","transparent");
+                    $(name+i+":empty").css("border-color","grey");
+                    $(name+i+"").css("border-color","blue");      
+                    
                 }
             }
-            if(!no)select=elem;
+            if(!no)
+            {
+               select=elem;
+               $(this).css("border-color","red");
+            }
+            
+            
             
         });
+        
 }
 
 
 function wmob()
 {
     
-    for(var i=0;looking("#wmob"+i+"");i++)
+    for(var i=0;looking(".wmob"+i+"");i++)
     {
-        selectObj("#wmob"+i+"","#wmob");
+        selectObj(".wmob"+i+"",".wmob");
         
     }
 }
 
 
-
+$(document).keydown(function(tecla){
+    if (tecla.keyCode === 46 &&(select!=="")) {
+       var s;
+       var x=1;
+       if(select!="#tablero")
+       {
+           $(select).addClass("descartado");
+           $(select+" *").addClass("descartado");
+           select=""; 
+       }
+       
+       
+       
+       
+       
+    }
+    if (tecla.keyCode === 83 &&select!=='#tablero') {
+       descartados=0;
+       for(var i=0;looking(".wmob"+i+"")&&x;i++)
+       {
+           if(isJQ(".wmob"+i+"",".descartado"))
+           {
+               descartados++;
+           }
+       }
+       if(descartados===objets)
+       {
+           $("#tablero").empty();
+           objets=0;
+       }
+       
+        
+    }
+});
 
 
 $(document).ready(function(){
@@ -50,14 +101,23 @@ $(document).ready(function(){
     $('#tablero').click(function()
     {
         no=0;
-        for(var i=0;looking("#wmob"+i+"");i++)
+        for(var i=0;looking(".wmob"+i+"");i++)
         {
-            if(isJQ("#wmob"+i+"",":hover"))
+            if(isJQ(".wmob"+i+"",":hover"))
             {
                 no++;
             }
+            else
+            {
+                $(".wmob"+i+"").css("border-color","transparent");
+                $(".wmob"+i+":empty").css("border-color","grey");
+                $(".wmob"+i+":hover").css("border-color","blue");
+            }
         }
-        if(!no)select='#tablero';
+        if(!no){
+            select='#tablero';
+            $(this).css("border-color","red");
+        }
         $(this).css({
             'heigth':'20px'
         });
@@ -65,20 +125,30 @@ $(document).ready(function(){
     
     $('#header').click(function()
     {
-        $(select).append("<header id='wmob"+objets+"'></header>");
-        var a=new wmob();
-        objets++;
+        if(select!=="")
+        {
+            $(select).append("<header class='wmob"+objets+"'></header>");
+            var a=new wmob();
+            objets++;
+        }
+        
     });    
     $('#article').click(function()
     {
-        $(select).append("<article id='wmob"+objets+"'></article>");
+         if(select!=="")
+        {
+        $(select).append("<article class='wmob"+objets+"'></article>");
         var a=new wmob();
         objets++;
+    }
     });    
     $('#section').click(function()
     {
-        $(select).append("<section id='wmob"+objets+"'></section>");
+         if(select!=="")
+        {
+        $(select).append("<section class='wmob"+objets+"'></section>");
          var a=new wmob();
         objets++;
+    }
     });
 });
